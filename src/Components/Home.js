6f291f2e69072b103353a3958b88289ba18f2b56
gamzenurtukel <gamzenurtukel@gmail.com>
2022-10-02 16:15:32 +0300

    home page edited

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,23 +2,23 @@ import React from "react";
 import Lottie from 'react-lottie';
 import lf30_editor_j15qrcmh from "../lottie/lf30_editor_j15qrcmh.json"
 
-function Home(){
-    return(
+function Home() {
+    return (
         <div>
-            <h2 style={{color:"#1890ff",fontSize:"30px"}}>WELCOME TO STARSHIPS PAGE</h2>            
-            <p style={{fontWeight:"500"}}>
-              Click "starships list" for a list of starships.
+            <h2 style={{ color: "#1890ff", fontSize: "30px" }}>WELCOME TO STARSHIPS PAGE</h2>
+            <p style={{ fontWeight: "500" }}>
+                Click "starships list" for a list of starships.
             </p>
-            <br/>
-      <Lottie
-        height={500}
-        width={400}
-        options={{
-          animationData: lf30_editor_j15qrcmh,
-          loop: true,
-          autoplay: true,
-        }}
-      />
+            <br />
+            <Lottie
+                height={500}
+                width={400}
+                options={{
+                    animationData: lf30_editor_j15qrcmh,
+                    loop: true,
+                    autoplay: true,
+                }}
+            />
         </div>
     );
 };
